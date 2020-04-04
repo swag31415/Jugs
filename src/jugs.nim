e10852a60ba52b081bab192430fd5d8f0bd6b800
swag31415 <kidswagatam18@gmail.com>
2020-04-04 19:04:31 -0400

    Added a proc for getting safe integer input

--- src/jugs.nim
+++ src/jugs.nim
@@ -1,2 +1,13 @@
 ## Jugs
 
+import strutils
+
+# Prompt the user and get positive non-zero integer input
+proc get_num(prompt: string): uint =
+  var input: uint
+  echo prompt
+  try:
+    input = readLine(stdin).parseUInt()
+  except ValueError:
+    input = get_num("Please enter a positive integer")
+  return input
