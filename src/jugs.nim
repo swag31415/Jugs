960e09ea9357af0b93b1d632470f49aa92387ba2
swag31415 <kidswagatam18@gmail.com>
2020-04-04 19:20:04 -0400

    Added input for jug sizes and target jug size

--- src/jugs.nim
+++ src/jugs.nim
@@ -11,3 +11,10 @@ proc get_num(prompt: string): uint =
   except ValueError:
     input = get_num("Please enter a positive integer")
   return input
+
+# Get all the jug sizes as well as the target amount
+let jug_count = get_num("How many jugs are there?")
+var jug_sizes: seq[uint]
+for i in 1..jug_count:
+  jug_sizes.add(get_num("How big is jug " & $i & "?"))
+let target = get_num("What is the target amount?")
