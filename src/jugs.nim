cd6ff7f6b1f6f412c85962c0d06d4f7526f75dae
swag31415 <kidswagatam18@gmail.com>
2020-04-12 17:11:49 -0400

    Implemented a bodgy BFS alg

--- src/jugs.nim
+++ src/jugs.nim
@@ -18,3 +18,26 @@ var jug_sizes: seq[uint]
 for i in 1..jug_count:
   jug_sizes.add(get_num("How big is jug " & $i & "?"))
 let target = get_num("What is the target amount?")
+
+var path: seq[uint]
+var log: seq[int]
+
+block search:
+  path.add(0)
+  var i = 0
+  while i < path.len():
+    for size in jug_sizes:
+      if not path.contains(path[i] + size):
+        path.add(path[i] + size)
+        log.add(i)
+        if path[i] + size == target: break search
+      if size < path[i] and not path.contains(path[i] - size):
+        path.add(path[i] - size)
+        log.add(i)
+        if path[i] - size == target: break search
+    inc(i)
+
+var i = log.len
+while i > 0:
+  echo path[i]
+  i = log[i - 1]
